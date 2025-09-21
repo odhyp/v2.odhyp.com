2a7ea595360be5ea2776774245f8b044fbdcd27c
odhyp <pradhana.odhy@gmail.com>
2025-09-21 13:10:17 +0700

    refactor: simplify schema definitions for writings and projects collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,34 +4,31 @@ import { glob } from "astro/loaders";
 // Writings
 const writings = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/writings" }),
-  schema: ({ image }) =>
-    z.object({
-      draft: z.boolean(),
-      title: z.string(),
-      description: z.string(),
-      pubDate: z.coerce.date(),
-      updatedDate: z.coerce.date(),
-      cover: image().optional(),
-      tags: z.array(z.string()).optional(),
-      toc: z.boolean().optional(),
-    }),
+  schema: z.object({
+    draft: z.boolean(),
+    type: z.enum(["article", "log"]),
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date(),
+    tags: z.array(z.string()).optional(),
+    toc: z.boolean().optional(),
+  }),
 });
 
 // Projects
 const projects = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/projects" }),
-  schema: ({ image }) =>
-    z.object({
-      draft: z.boolean(),
-      title: z.string(),
-      description: z.string(),
-      pubDate: z.coerce.date(),
-      updatedDate: z.coerce.date(),
-      cover: image().optional(),
-      tags: z.array(z.string()).optional(),
-      toc: z.boolean().optional(),
-      version: z.string().optional(),
-    }),
+  schema: z.object({
+    draft: z.boolean(),
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date(),
+    tags: z.array(z.string()).optional(),
+    toc: z.boolean().optional(),
+    version: z.string().optional(),
+  }),
 });
 
 // Photos
