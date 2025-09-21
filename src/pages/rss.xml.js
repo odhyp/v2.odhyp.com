193f22d52a913a5b4bd66f53fa2a734cc85854ea
odhyp <pradhana.odhy@gmail.com>
2025-09-21 13:10:25 +0700

    refactor: update RSS feed to use writings collection instead of articles

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,17 +2,17 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const articles = await getCollection("articles");
+  const writings = await getCollection("writings");
   return rss({
     title: "Odhy Pradhana",
     description:
       "Thoughts and lessons on programming, web development, and the things I learn along the way.",
     site: context.site,
-    items: articles.map((post) => ({
+    items: writings.map((post) => ({
       title: post.data.title,
       description: post.data.description,
       pubDate: post.data.updatedDate,
-      link: `/articles/${post.id}/`,
+      link: `/writings/${post.id}/`,
     })),
   });
 }
