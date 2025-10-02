488720b505a850b02cb1cce20d07bd2f5a97ac76
odhyp <pradhana.odhy@gmail.com>
2025-10-02 18:56:19 +0700

    refactor: update expressive code frame color using flexoki theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,9 +28,18 @@ export default defineConfig({
       themes: "material-theme-darker",
       styleOverrides: {
         codeFontFamily: "Geist Mono, monospace",
-        codeFontSize: "0.9rem",
         uiFontFamily: "Geist, sans-serif",
-        frames: {},
+        borderColor: "#343331", // ui-2
+        frames: {
+          editorBackground: "#100f0f", // bg
+          editorActiveTabBackground: "#282726", // ui
+          editorActiveTabIndicatorBottomColor: "#cecdc3", // tx
+          editorTabBarBackground: "#1c1b1a", // bg-2
+          editorTabBarBorderColor: "#343331", // ui-2
+          editorTabBarBorderBottomColor: "#343331", // ui-2
+          terminalBackground: "#100f0f", // bg
+          terminalTitlebarBackground: "#282726", // ui
+        },
       },
       plugins: [pluginLineNumbers()],
       defaultProps: {
