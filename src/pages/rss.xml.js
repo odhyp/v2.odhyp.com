305c95e9eda89d86779223205530a6325c0b5307
odhyp <pradhana.odhy@gmail.com>
2025-11-10 14:37:08 +0700

    fix: update writings RSS feed link

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,7 +12,7 @@ export async function GET(context) {
       title: post.data.title,
       description: post.data.description,
       pubDate: post.data.updatedDate,
-      link: `/writings/${post.id}/`,
+      link: `/${post.id}`,
     })),
   });
 }
